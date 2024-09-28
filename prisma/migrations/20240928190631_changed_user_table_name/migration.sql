c9ae42942684cb328e1a20e6b616f06d81ece291
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-28 23:05:02 +0200

    made new migration changed table name from user to users

---
+++ prisma/migrations/20240928190631_changed_user_table_name/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "file" DROP CONSTRAINT "file_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "folder" DROP CONSTRAINT "folder_userId_fkey";
+
+-- DropTable
+DROP TABLE "user";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- AddForeignKey
+ALTER TABLE "file" ADD CONSTRAINT "file_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "folder" ADD CONSTRAINT "folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
