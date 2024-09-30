7f971fb21aba4edb1a45dc3b5921835829470dc0
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-30 12:04:36 +0200

    added passport config file

---
+++ middlewares/passport.js
@@ -0,0 +1,37 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const db = require("../db/queries");
+
+const strategy = new LocalStrategy(async function (username, password, done) {
+  try {
+    const user = await db.findUser(username);
+
+    if (!user) {
+      return done(null, false, { message: "Incorrect username!" });
+    }
+    const match = bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return done(null, false, { message: "Incorrect password!" });
+    }
+    return done(null, user);
+  } catch (err) {
+    console.log(err);
+    return done(err);
+  }
+});
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.deserializeUser(id);
+    console.log(user, "desirilize");
+
+    return done(null, user);
+  } catch (err) {}
+});
