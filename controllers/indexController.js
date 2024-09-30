3dae2f2a06eab92366820387e7608a228631fbf7
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-01 00:17:06 +0200

    added indexController with getIndex middleware

---
+++ controllers/indexController.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
+const getIndex = asyncHandler(async (req, res, next) => {
+  if (res.locals.currentUser)
+    res.render("index", {
+      title: "CloudUp - Homepage",
+      user: req.user,
+    });
+  else {
+    res.render("log-in", {
+      title: "CloudUp - Homepage",
+      user: req.user,
+    });
+  }
+});
+
+module.exports = {
+  getIndex,
+};
