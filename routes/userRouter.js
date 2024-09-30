7a0a23d9fc720761e5d1e576a6a00ec6e68536e1
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-09-30 12:05:13 +0200

    added authenticate for local strategy in / route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
+const passport = require("passport");
 
 const userController = require("../controllers/userController");
 const userRouter = Router();
 
 userRouter.get("/", userController.getLogIn);
+
+userRouter.post(
+  "/",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  })
+);
 
 userRouter.get("/sign-up", userController.getSignUp);
 
