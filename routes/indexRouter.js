1e1630ff22c44cd068dbf7c833a91684fea979a9
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-01 11:08:47 +0200

    added post route for multer file uploads. allowed multiple files to be added into form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "./uploads" }); // change to cloud based storage
 
 const indexController = require("../controllers/indexController");
 
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getIndex);
+
+indexRouter.post("/", upload.array("file_upload"), (req, res, next) => {});
 
 module.exports = indexRouter;
