0b9ae7a39e85f14232cdd5a18cecfbf599999d83
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-07 23:06:53 +0200

    passed currentUrl, currentFolder to views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,14 +3,15 @@ const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
 const getIndex = asyncHandler(async (req, res, next) => {
+  const currentUrl = req.url;
   const folders = await db.getFolders(null);
-  const parent = req.params.name;
 
   if (res.locals.currentUser) {
     res.render("index", {
       title: "CloudUp - Homepage",
       folders: folders,
-      parent: parent,
+      currentUrl: currentUrl,
+      currentFolder: "/",
     });
   } else {
     res.redirect("/log-in");
