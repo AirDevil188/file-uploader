6ebae7adad8e0d2f0f803ce1c995cbe438ac458f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-10 23:57:29 +0200

    deleted redudant deleteFolder query. added try&catch blocks to queries

--- db/queries.js
+++ db/queries.js
@@ -3,88 +3,120 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function deserializeUser(id) {
-  const user = await prisma.users.findUnique({
-    where: {
-      id: id,
-    },
-  });
-  return user;
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 async function findUser(username) {
-  const user = await prisma.users.findUnique({
-    where: { username: username },
-  });
-  return user;
+  try {
+    const user = await prisma.users.findUnique({
+      where: { username: username },
+    });
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 async function createUser(username, password) {
-  await prisma.users.create({
-    data: {
-      username: username,
-      password: password,
-    },
-  });
+  try {
+    await prisma.users.create({
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 async function createFolder(name, id, parent) {
-  if (parent) {
-    await prisma.folder.create({
-      data: {
-        name: name,
-        userId: id,
-        parentId: parent,
-      },
-    });
-  } else {
-    await prisma.folder.create({
-      data: {
-        name: name,
-        userId: id,
-      },
-    });
+  try {
+    if (parent) {
+      await prisma.folder.create({
+        data: {
+          name: name,
+          userId: id,
+          parentId: parent,
+        },
+      });
+    } else {
+      await prisma.folder.create({
+        data: {
+          name: name,
+          userId: id,
+        },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 }
 
 async function deleteFolder(id) {
-  await prisma.folder.delete({
-    where: { id: id },
-  });
+  try {
+    await prisma.folder.delete({
+      where: { id: id },
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 async function updateFolder(id, name) {
-  await prisma.folder.update({
-    where: { id: id },
-    data: {
-      name: name,
-    },
-  });
+  try {
+    await prisma.folder.update({
+      where: { id: id },
+      data: {
+        name: name,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 async function getFolders(id) {
-  if (!id) {
-    return await prisma.folder.findMany({
-      where: { parent: id },
-    });
-  } else {
-    return await prisma.folder.findMany({
-      where: { parentId: id },
-    });
+  try {
+    if (!id) {
+      return await prisma.folder.findMany({
+        where: { parent: id },
+      });
+    } else {
+      return await prisma.folder.findMany({
+        where: { parentId: id },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 }
 
 async function getFolder(id) {
-  return await prisma.folder.findFirst({
-    where: { id: id },
-  });
-}
-
-async function deleteFolder(id) {
-  return await prisma.folder.delete({
-    where: {
-      id: id,
-    },
-  });
+  try {
+    return await prisma.folder.findFirst({
+      where: { id: id },
+    });
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
 }
 
 module.exports = {
