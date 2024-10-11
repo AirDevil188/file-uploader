ea2923b50db503072eecb1965524ed1051ec5c84
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-11 13:28:29 +0200

    added createDriveFolder middleware

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,18 +2,31 @@ const multer = require("multer");
 const asyncHandler = require("express-async-handler");
 const db = require("../db/queries");
 
+const createDriveFolder = asyncHandler(async (req, res, next) => {
+  const driveFolder = await db.getDriveFolder("drive");
+  if (!driveFolder) {
+    await db.createDriveFolder(res.locals.currentUser.id);
+    const driveFolder = await db.getDriveFolder("drive");
+    res.redirect(`drive/${driveFolder.id}`);
+  }
+  res.redirect(`drive/${driveFolder.id}`);
+});
+
 const getIndex = asyncHandler(async (req, res, next) => {
+  const driveFolder = await db.getFolder(req.params.id);
   const currentUrl = req.url;
-  const folders = await db.getFolders(null);
+  const folders = await db.getFolders(driveFolder.id);
+  console.log(req.params.id);
 
   res.render("index", {
     title: "CloudUp - Homepage",
     folders: folders,
     currentUrl: currentUrl,
-    currentFolder: "/",
+    currentFolder: driveFolder,
   });
 });
 
 module.exports = {
   getIndex,
+  createDriveFolder,
 };
