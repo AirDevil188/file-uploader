d845346360379f903acf283c2a49226ca1caee30
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-12 18:36:52 +0200

    removed multer from indexRouter and multer route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,4 @@
 const { Router } = require("express");
-const multer = require("multer");
-const upload = multer({ dest: "./uploads" }); // change to cloud based storage
 
 const indexController = require("../controllers/indexController");
 const folderController = require("../controllers/folderController");
@@ -13,11 +11,5 @@ indexRouter.get("/", isAuth.isAuth, indexController.createDriveFolder);
 indexRouter.get("/drive/:id", isAuth.isAuth, indexController.getIndex);
 
 indexRouter.post("/drive/:id", isAuth.isAuth, folderController.postFolder);
-
-indexRouter.post(
-  "drive/file-upload/:id",
-  upload.array("file_upload"),
-  (req, res, next) => {}
-);
 
 module.exports = indexRouter;
