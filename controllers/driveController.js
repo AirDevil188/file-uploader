a18e406713e5dd8a69828cac6947d7881506158a
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-10-12 21:25:07 +0200

    added getDrive middleware for folders and files

--- controllers/driveController.js
+++ controllers/driveController.js
@@ -12,6 +12,33 @@ const createDriveFolder = asyncHandler(async (req, res, next) => {
   res.redirect(`drive/${driveFolder.id}`);
 });
 
+const getDrive = asyncHandler(async (req, res, next) => {
+  const currentUrl = req.url;
+  const subFolders = await db.getFolders(
+    req.params.id,
+    res.locals.currentUser.id
+  );
+  const currentFolder = await db.getFolder(
+    req.params.id,
+    res.locals.currentUser.id
+  );
+
+  const files = await db.getFiles(req.params.id, res.locals.currentUser.id);
+
+  if (!currentFolder) {
+    return res.status(404).render("404");
+  }
+
+  return res.render("index", {
+    title: `${req.params.name} Folder`,
+    folders: subFolders,
+    files: files,
+    currentFolder: currentFolder,
+    currentUrl: currentUrl,
+  });
+});
+
 module.exports = {
+  getDrive,
   createDriveFolder,
 };
